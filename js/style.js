ac2f518ecbbc32330c8f95afba782f0acdb6f600
faithseasonsdesign <67489226+faithseasonsdesign@users.noreply.github.com>
2022-01-25 00:56:56 +0200

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,34 @@
+
+
+function navigationButton(){
+    $('.floating-button-wrapper').click(function(){
+        $(".floating-action-button-menu").slideToggle(400);
+    });
+}
+
+function hideMenu(){
+    $(".about-link").click(function(){
+        $(".navigation-list-outer-wrapper").hide();
+    })
+}
+
+function styling(){
+
+    let window_width = window.outerWidth;
+    let window_height = window.outerHeight;
+
+    if(window_height <= 568){
+        let section = document.getElementsByClassName("background-img-wrapper");
+        let mask = document.getElementsByClassName("bg-image-mask");
+        //mask[0].style.height = "110vh";
+        //section[0].style.height ="110vh";
+        //welcome_caption[0].style.fontSize = "14pt";
+    }
+   
+}
+
+
+
+navigationButton();
+styling();
+//cardButtons();
